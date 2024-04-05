a2cc2da2e667ba12e6db4e6445b22cfc505f1a47
a.burgart02 <a.burgart02@gmail.com>
2024-04-05 23:10:30 +0500

    Fix

--- Source/Kontur.BigLibrary.Tests.Integration/BookServiceTests/BookServiceMockTest.cs
+++ Source/Kontur.BigLibrary.Tests.Integration/BookServiceTests/BookServiceMockTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -17,29 +16,19 @@ namespace Kontur.BigLibrary.Tests.Integration.BookServiceTests;
 
 public class BookServiceMockTest
 {
-    private static IServiceProvider _container;
-    private static IBookService _bookService;
-    private static IBookRepository _bookRepository;
-    private static IImageRepository _imageRepository;
-    private static IEventRepository _eventRepository;
-    
     private const string Name = "Database Systems. The Complete Book";
     private const string Author = "Hector Garcia-Molina, Jeffrey D.Ullman, Jennifer Widom";
     private const string Description = "New_book";
 
-    [SetUp]
-    public void SetUp()
-    {
-        _container = new ContainerWithMocks().Build();
-        _bookService = _container.GetRequiredService<IBookService>();
-        _bookRepository = _container.GetRequiredService<IBookRepository>();
-        _imageRepository = _container.GetRequiredService<IImageRepository>();
-        _eventRepository = _container.GetRequiredService<IEventRepository>();
-    }
-    
     [Test]
     public async Task SaveBookAsync_ReturnSameBook_WhenSaveCorrectBook()
     {
+        var container = new ContainerWithMocks().Build();
+        var bookService = container.GetRequiredService<IBookService>();
+        var bookRepository = container.GetRequiredService<IBookRepository>();
+        var imageRepository = container.GetRequiredService<IImageRepository>();
+        var eventRepository = container.GetRequiredService<IEventRepository>();
+        
         var correctBook = new Book()
         {
             Name = Name,
@@ -47,21 +36,27 @@ public class BookServiceMockTest
             Description = Description
         };
         
-        _imageRepository?.GetAsync(correctBook.ImageId,Arg.Any<CancellationToken>()).Returns(new Image());
-        _bookRepository?.GetRubricAsync(correctBook.RubricId, CancellationToken.None).Returns(new Rubric());
-        _bookRepository?.SaveBookAsync(correctBook, CancellationToken.None).Returns(correctBook);
+        imageRepository?.GetAsync(correctBook.ImageId,Arg.Any<CancellationToken>()).Returns(new Image());
+        bookRepository?.GetRubricAsync(correctBook.RubricId, CancellationToken.None).Returns(new Rubric());
+        bookRepository?.SaveBookAsync(correctBook, CancellationToken.None).Returns(correctBook);
         
-        var result = await _bookService.SaveBookAsync(correctBook, CancellationToken.None);
+        var result = await bookService.SaveBookAsync(correctBook, CancellationToken.None);
         result.Should().BeEquivalentTo(correctBook);
-        await _bookRepository.Received()?.SaveBookAsync(correctBook, CancellationToken.None)!;
-        await _bookRepository.Received()?.SaveBookIndexAsync(Arg.Any<int>(), correctBook.GetTextForFts(),
+        await bookRepository.Received(1)?.SaveBookAsync(correctBook, CancellationToken.None)!;
+        await bookRepository.Received(1)?.SaveBookIndexAsync(Arg.Any<int>(), correctBook.GetTextForFts(),
             Arg.Any<string>(), CancellationToken.None)!;
-        await _eventRepository.Received()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
+        await eventRepository.Received(1)?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
     }
     
     [Test]
     public async Task SaveBookAsync_ThrowValidationException_WhenRequiredFieldIsNull()
     {
+        var container = new ContainerWithMocks().Build();
+        var bookService = container.GetRequiredService<IBookService>();
+        var bookRepository = container.GetRequiredService<IBookRepository>();
+        var imageRepository = container.GetRequiredService<IImageRepository>();
+        var eventRepository = container.GetRequiredService<IEventRepository>();
+        
         var bookWithNullRequiredField = new Book()
         {
             Name = null,
@@ -69,25 +64,31 @@ public class BookServiceMockTest
             Description = Description
         };
         
-        _imageRepository?.GetAsync(bookWithNullRequiredField.ImageId, Arg.Any<CancellationToken>())
+        imageRepository?.GetAsync(bookWithNullRequiredField.ImageId, Arg.Any<CancellationToken>())
             .Returns(new Image());
-        _bookRepository?.GetRubricAsync(bookWithNullRequiredField.RubricId, CancellationToken.None)
+        bookRepository?.GetRubricAsync(bookWithNullRequiredField.RubricId, CancellationToken.None)
             .Returns(new Rubric());
-        _bookRepository?.SaveBookAsync(bookWithNullRequiredField, CancellationToken.None)
+        bookRepository?.SaveBookAsync(bookWithNullRequiredField, CancellationToken.None)
             .Returns(bookWithNullRequiredField);
         
-        var exception = Assert.ThrowsAsync<ValidationException>(() => _bookService
+        var exception = Assert.ThrowsAsync<ValidationException>(() => bookService
             .SaveBookAsync(bookWithNullRequiredField, CancellationToken.None));
         Assert.That(exception.Message, Is.EqualTo("Не заполнены обязательные поля"));
-        await _bookRepository.DidNotReceive()?.SaveBookAsync(bookWithNullRequiredField, CancellationToken.None)!;
-        await _bookRepository.DidNotReceive()?.SaveBookIndexAsync(Arg.Any<int>(), bookWithNullRequiredField.GetTextForFts(),
+        await bookRepository.DidNotReceive()?.SaveBookAsync(bookWithNullRequiredField, CancellationToken.None)!;
+        await bookRepository.DidNotReceive()?.SaveBookIndexAsync(Arg.Any<int>(), bookWithNullRequiredField.GetTextForFts(),
             Arg.Any<string>(), CancellationToken.None)!;
-        await _eventRepository.DidNotReceive()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
+        await eventRepository.DidNotReceive()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
     }
     
     [Test]
     public async Task SaveBookAsync_ThrowValidationException_WhenRubricIsNotExisting()
     {
+        var container = new ContainerWithMocks().Build();
+        var bookService = container.GetRequiredService<IBookService>();
+        var bookRepository = container.GetRequiredService<IBookRepository>();
+        var imageRepository = container.GetRequiredService<IImageRepository>();
+        var eventRepository = container.GetRequiredService<IEventRepository>();
+        
         var bookWithNotExistingRubric = new Book()
         {
             Name = Name,
@@ -95,25 +96,31 @@ public class BookServiceMockTest
             Description = Description
         };
         
-        _imageRepository?.GetAsync(bookWithNotExistingRubric.ImageId,Arg.Any<CancellationToken>())
+        imageRepository?.GetAsync(bookWithNotExistingRubric.ImageId,Arg.Any<CancellationToken>())
             .Returns(new Image());
-        _bookRepository?.GetRubricAsync(bookWithNotExistingRubric.RubricId, CancellationToken.None)
+        bookRepository?.GetRubricAsync(bookWithNotExistingRubric.RubricId, CancellationToken.None)
             .Returns((Rubric)null);
-        _bookRepository?.SaveBookAsync(bookWithNotExistingRubric, CancellationToken.None)
+        bookRepository?.SaveBookAsync(bookWithNotExistingRubric, CancellationToken.None)
             .Returns(bookWithNotExistingRubric);
         
-        var exception = Assert.ThrowsAsync<ValidationException>(() => _bookService
+        var exception = Assert.ThrowsAsync<ValidationException>(() => bookService
             .SaveBookAsync(bookWithNotExistingRubric, CancellationToken.None));
         Assert.That(exception.Message, Is.EqualTo("Указана несуществующая рубрика."));
-        await _bookRepository.DidNotReceive()?.SaveBookAsync(bookWithNotExistingRubric, CancellationToken.None)!;
-        await _bookRepository.DidNotReceive()?.SaveBookIndexAsync(Arg.Any<int>(), bookWithNotExistingRubric.GetTextForFts(),
+        await bookRepository.DidNotReceive()?.SaveBookAsync(bookWithNotExistingRubric, CancellationToken.None)!;
+        await bookRepository.DidNotReceive()?.SaveBookIndexAsync(Arg.Any<int>(), bookWithNotExistingRubric.GetTextForFts(),
             Arg.Any<string>(), CancellationToken.None)!;
-        await _eventRepository.DidNotReceive()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
+        await eventRepository.DidNotReceive()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
     }
     
     [Test]
     public async Task SaveBookAsync_ThrowValidationException_WhenImageIsNotExisting()
     {
+        var container = new ContainerWithMocks().Build();
+        var bookService = container.GetRequiredService<IBookService>();
+        var bookRepository = container.GetRequiredService<IBookRepository>();
+        var imageRepository = container.GetRequiredService<IImageRepository>();
+        var eventRepository = container.GetRequiredService<IEventRepository>();
+        
         var bookWithNotExistingImage = new Book()
         {
             Name = Name,
@@ -121,19 +128,19 @@ public class BookServiceMockTest
             Description = Description
         };
         
-        _imageRepository?.GetAsync(bookWithNotExistingImage.ImageId,Arg.Any<CancellationToken>())
+        imageRepository?.GetAsync(bookWithNotExistingImage.ImageId,Arg.Any<CancellationToken>())
             .Returns((Image)null);
-        _bookRepository?.GetRubricAsync(bookWithNotExistingImage.RubricId, CancellationToken.None)
+        bookRepository?.GetRubricAsync(bookWithNotExistingImage.RubricId, CancellationToken.None)
             .Returns(new Rubric());
-        _bookRepository?.SaveBookAsync(bookWithNotExistingImage, CancellationToken.None)
+        bookRepository?.SaveBookAsync(bookWithNotExistingImage, CancellationToken.None)
             .Returns(bookWithNotExistingImage);
         
-        var exception = Assert.ThrowsAsync<ValidationException>(() => _bookService
+        var exception = Assert.ThrowsAsync<ValidationException>(() => bookService
             .SaveBookAsync(bookWithNotExistingImage, CancellationToken.None));
         Assert.That(exception.Message, Is.EqualTo("Указана несуществующая картинка."));
-        await _bookRepository.DidNotReceive()?.SaveBookAsync(bookWithNotExistingImage, CancellationToken.None)!;
-        await _bookRepository.DidNotReceive()?.SaveBookIndexAsync(Arg.Any<int>(), bookWithNotExistingImage.GetTextForFts(),
+        await bookRepository.DidNotReceive()?.SaveBookAsync(bookWithNotExistingImage, CancellationToken.None)!;
+        await bookRepository.DidNotReceive()?.SaveBookIndexAsync(Arg.Any<int>(), bookWithNotExistingImage.GetTextForFts(),
             Arg.Any<string>(), CancellationToken.None)!;
-        await _eventRepository.DidNotReceive()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
+        await eventRepository.DidNotReceive()?.SaveAsync(Arg.Any<ChangedEvent>(), CancellationToken.None)!;
     }
 }
--- Source/Kontur.BigLibrary.Tests.Integration/BookServiceTests/ContainerWithMocks.cs
+++ Source/Kontur.BigLibrary.Tests.Integration/BookServiceTests/ContainerWithMocks.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Data;
-using System.Data.SQLite;
-using System.Threading;
-using System.Threading.Tasks;
-using Kontur.BigLibrary.DataAccess;
 using Kontur.BigLibrary.Service.Services.BookService;
 using Kontur.BigLibrary.Service.Services.BookService.Repository;
 using Kontur.BigLibrary.Service.Services.EventService;
@@ -19,20 +14,10 @@ namespace Kontur.BigLibrary.Tests.Integration.BookServiceTests;
 public class ContainerWithMocks
 {
     public IServiceCollection _collection;
-
-    public class MockDataBase : IDbConnectionFactory
-    {
-        public async Task<IDbConnection> OpenAsync(CancellationToken cancellation)
-        {
-            return new SQLiteConnection();
-        }
-    }
     
     public ContainerWithMocks()
     {
         _collection = new ServiceCollection();
-        
-        _collection.AddSingleton<IDbConnectionFactory, MockDataBase>();
 
         _collection.AddSingleton<ISynonymMaker, SynonymMaker>();
 
